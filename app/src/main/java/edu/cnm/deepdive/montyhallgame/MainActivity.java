450222c60a272daf35638ec58a73331d6000d982
Matthew Morgan <mmorgan55@cnm.edu>
2018-10-02 12:13:28 -0600

    Basic layout started.

--- app/src/main/java/edu/cnm/deepdive/montyhallgame/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/montyhallgame/MainActivity.java
@@ -2,12 +2,16 @@ package edu.cnm.deepdive.montyhallgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+  private TextView winTally;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    winTally = findViewById(R.id.win_tally);
   }
 }
